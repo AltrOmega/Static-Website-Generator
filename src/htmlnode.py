7250c90fbb99c6f53edadff726f1cdda9d902985
AltrOmega <michal.szalachowski.33@gmail.com>
2025-05-12 14:05:47 +0200

    feat: add LeafNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,3 @@
-
-
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
@@ -10,22 +8,40 @@ class HTMLNode:
 
 
     def to_html(self):
-        raise NotImplementedError("Override this funcion inside HTMLNode's children")
+        raise NotImplementedError("Override this funcion inside HTMLNode's children.")
 
 
 
     def props_to_html(self):
         text = ""
 
-        if len(self.props) == 0: return ""
+        if self.props == None: return ""
 
         for key, value in self.props.items():
-                text += f'{key}="{value}" '
+                text += f' {key}="{value}"'
 
-        return text.rstrip()
+        return text
 
 
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.props_to_html()}, {self.children})"
 
+
+
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, children=None, props=props)
+    
+    # Override
+    def to_html(self):
+        if self.value == None or self.value == '':
+            raise ValueError("LeafNode has to have a value.")
+        
+        if self.tag == None:
+            return self.value
+
+        text = ""
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,15 +1,34 @@
 import unittest
-from htmlnode import HTMLNode 
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
         tests = [
-                (HTMLNode(props={'sus': 'amongus'}).props_to_html(), 'sus="amongus"'),
-                (HTMLNode(props={'sus': 'amongus', 'baller': 'ballyn'}).props_to_html(), 'sus="amongus" baller="ballyn"'),
+                (HTMLNode(props={'sus': 'amongus'}).props_to_html(), ' sus="amongus"'),
+                (HTMLNode(props={'sus': 'amongus', 'baller': 'ballyn'}).props_to_html(), ' sus="amongus" baller="ballyn"'),
                 (HTMLNode(props={
                                 'sus': 'amongus', 'baller': 'ballyn', 'misione': 'imposible'}).props_to_html(),
-                                'sus="amongus" baller="ballyn" misione="imposible"'
+                                ' sus="amongus" baller="ballyn" misione="imposible"'
                 ),
+        ]
+
+        for test in tests:
+            self.assertEqual(test[0], test[1])
+
+
+
+    def test_leaf_to_html_p(self):
+        tests = [
+            (LeafNode("p", "Hello, world!").to_html(), "<p>Hello, world!</p>"),
+            (LeafNode("h1", "SCREAMING EAGLES").to_html(), "<h1>SCREAMING EAGLES</h1>"),
+
+            (LeafNode("a", "Google link",
+                props={'href': 'https://www.google.com'}).to_html(),
+                '<a href="https://www.google.com">Google link</a>'),
+
+            (LeafNode("a", "Google link ext",
+                props={'href': 'https://www.google.com', 'ext': 'VOID'}).to_html(),
+                '<a href="https://www.google.com" ext="VOID">Google link ext</a>'),
         ]
 
         for test in tests:
